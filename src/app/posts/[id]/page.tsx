039439a6f3f21d377511ec7325f7cb77cec107cc
apurva1414 <apurva.jsdev@gmail.com>
2025-01-20 11:52:20 +0530

    feat: dynamic seo meta data

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,10 +1,27 @@
 import { PostDetails } from "@/ui";
-import type { Metadata } from "next";
+import axiosInstance from "@/libs/axios";
 
-export const metadata: Metadata = {
-    title: 'Home - Posts',
-    description: 'Browse through our collection of posts.',
-};
+export async function generateMetadata({ params }: { params: { id: string } }) {
+    const { id } = params;
+    try {
+        const response = await axiosInstance.get(`/posts/${id}`);
+        const post = response.data;
+        return {
+            title: post?.title || "Post Details",
+            description: post?.body || "Read more about this post.",
+            type: "article",
+            url: `https://posts-fe.netlify.app/posts/${id}`,
+        };
+    } catch (error) {
+        console.error("Failed to fetch post data:", error);
+
+        return {
+            title: "Post Not Found",
+            description: "Unable to fetch details for this post.",
+        };
+    }
+}
+
 
 export default function Home() {
     return (
