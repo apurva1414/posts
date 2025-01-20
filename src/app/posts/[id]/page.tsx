d522bb6a2c6c613be854145e6052f4f88456914b
apurva1414 <apurva.jsdev@gmail.com>
2025-01-20 12:12:21 +0530

    fix: dynamic seo meta data

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,16 +1,24 @@
+import { Metadata } from "next";
 import { PostDetails } from "@/ui";
 import axiosInstance from "@/libs/axios";
+import { PageProps } from "../../../../.next/types/app/posts/[id]/page";
 
-export async function generateMetadata({ params }: { params: { id: string } }) {
-    const { id } = params;
+
+export async function generateMetadata(pageProps: PageProps): Promise<Metadata> {
+    const { id } = await pageProps.params;
+
     try {
         const response = await axiosInstance.get(`/posts/${id}`);
         const post = response.data;
         return {
             title: post?.title || "Post Details",
             description: post?.body || "Read more about this post.",
-            type: "article",
-            url: `https://posts-fe.netlify.app/posts/${id}`,
+            openGraph: {
+                type: "article",
+                url: `https://posts-fe.netlify.app/posts/${id}`,
+                title: post?.title,
+                description: post?.body,
+            },
         };
     } catch (error) {
         console.error("Failed to fetch post data:", error);
